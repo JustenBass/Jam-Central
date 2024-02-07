96eb80aa8b4ec3b120f9b706a7499ef39c6ac07b
Justin Bass <justinbass6036@gmail.com>
2024-02-06 21:26:48 -0500

    Generated serializers for each model, pushed my migrations, and pushed seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,5 +7,6 @@ puts "Seeding Albums..."
 album1 = Album.create!(album: 'Midnights', year: '2023', artist_id: artist1.id, trending: true)
 
 puts "Seeding Songs..."
-song1 = Song.create!(song: 'Karma', views: '661,790,640', time: '3:24', atrist_id: artist.id, album_id: album1.id, popular: true)
+song1 = Song.create!(song: 'Karma', views: '661,790,640', time: '3:24', artist_id: artist1.id, album_id: album1.id, popular: true)
 
+puts "Done Seeing...🌷"
