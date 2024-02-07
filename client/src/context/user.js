a432bf60daa46716e99117251b07a90eff36cdb7
Justin Bass <justinbass6036@gmail.com>
2024-02-07 14:15:19 -0500

    Set state for a new user in my user context component. This will set me up for sign up functionality as well prepare me for Active Mailer.

--- client/src/context/user.js
+++ client/src/context/user.js
@@ -3,11 +3,18 @@ import React, { useState, useEffect } from "react";
 const UserContext = React.createContext()
 
 function UserProvider({ children }) {
+    const [ user, setUser ] = useState( null );
+    const [ isAuthenticated, setIsAuthenticated ] = useState( false )
+
+    const signup = ( user ) => {
+        setUser( user )
+        setIsAuthenticated( true )
+    }
 
 
     return(
         <UserContext.Provider
-        value={{ }}>
+        value={{ user, setUser, isAuthenticated, setIsAuthenticated, signup }}>
             {children}
         </UserContext.Provider>
     )
